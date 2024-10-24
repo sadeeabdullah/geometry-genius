54146fd7eccb49b1ee151eb2680eb0d84c393667
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-10-24 09:40:05 +0600

    initializing a dialog to show the value

--- js/index.js
+++ js/index.js
@@ -11,4 +11,18 @@ const h = document.getElementById('input-h').value
 document.getElementById('calculate-area').addEventListener('click',function(){
     console.log('hello clicked me')
     calculateTriangleArea(b,h);
+    const dialouge = document.createElement('dialog')
+    
+    // Set inner content for the dialog
+    dialouge.innerHTML = `
+      <h3>Calculation Result</h3>
+      <p>The area of the triangle is: ${calculateTriangleArea(b, h)}</p>
+      <button id="close-dialog">Close</button>
+    `;
+    
+    // Append the dialog to the body
+    document.body.appendChild(dialouge);
+    
+    // Show the dialog
+    dialouge.showModal();
 })
